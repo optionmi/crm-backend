40214f4c3ad157068bb2f71b6499e61b2573b4e3
Vikalp Mishra <vikalpmishra783@gmail.com>
2023-09-15 09:36:29 +0530

    code cleanup

--- controllers/Publishers/publisherController.js
+++ controllers/Publishers/publisherController.js
@@ -1,13 +1,7 @@
-const Sequelize = require("sequelize");
-const Op = Sequelize.Op;
 const bcrypt = require("bcrypt");
-// const User = require('../../models/Auth/user');
-// const Publisher = require('../../models/Publishers/publisher');
 const { PrismaClient } = require("@prisma/client");
-
 const prisma = new PrismaClient();
 
-// Create a new publisher and user
 const createPublisher = async (req, res) => {
     const {
         company_name,
@@ -29,7 +23,6 @@ const createPublisher = async (req, res) => {
         }
 
         // Check if a user with the same email already exists
-        // const existingUser = await User.findOne({ where: { email } });
         const existingUser = await prisma.users.findUnique({
             where: { email },
         });
@@ -42,15 +35,9 @@ const createPublisher = async (req, res) => {
         // Hash the password
         const hashedPassword = await bcrypt.hash(password, 11);
 
-        // Create the user in the Users table
-        // const user = await User.create({
-        //     email: email,
-        //     password: hashedPassword,
-        //     user_type: "publisher",
-        // });
         const user = await prisma.users.create({
             data: {
-                email: email,
+                email,
                 password: hashedPassword,
                 user_type: "publisher",
             },
@@ -60,8 +47,6 @@ const createPublisher = async (req, res) => {
             return res.status(500).json({ message: "User creation failed" });
         }
 
-        // Create the publisher using the user's ID
-        // const publisher = await Publisher.create({
         const publisher = await prisma.create({
             data: {
                 user_id: user.id,
@@ -84,50 +69,41 @@ const createPublisher = async (req, res) => {
     }
 };
 
-// Function to fetch publisher data (accessible to admin and publisher)
 const getPublisherData = async (req, res) => {
     try {
-        const publisherId = req.user.publisher_id; // Get publisher_id from the logged-in user
+        const id = parseInt(req.params.id);
+        if (isNaN(id)) {
+            res.status(400).send({ message: "Invalid ID" });
+            return;
+        }
 
-        // Check if the user is an admin or a publisher
-        if (req.user.user_type === "publisher") {
-            // Find the publisher data by publisher_id
-            // const publisher = await Publisher.findByPk(publisherId);
-            const publisher = await prisma.publisher.findUnique({
-                where: { id: publisherId },
-            });
+        const publisher = await prisma.publishers.findUnique({
+            where: { id },
+        });
 
-            if (publisher) {
-                return res.status(200).json(publisher);
-            } else {
-                return res
-                    .status(404)
-                    .json({ message: "Publisher data not found" });
-            }
+        if (publisher) {
+            return res.status(200).json(publisher);
         } else {
-            return res.status(403).json({ message: "Permission denied" });
+            return res
+                .status(404)
+                .json({ message: "Publisher data not found" });
         }
     } catch (error) {
-        // Handle errors appropriately
         console.error(error);
         res.status(500).send("Server Error");
     }
 };
 
-// Function to fetch all publishers (accessible to admin only)
 const getAllPublishers = async (req, res) => {
     try {
-        // Check if the user is an admin
         if (req.user.user_type !== "admin") {
             return res.status(403).json({ message: "Permission denied" });
         }
 
-        // Fetch all publishers
-        // const publishers = await Publisher.findAll();
         const publishers = await prisma.publishers.findMany();
 
         if (publishers.length > 0) {
-            return res.status(200).json(publishers);
+            return res.status(200).json({ data: publishers });
         } else {
             return res.status(404).json({ message: "No publishers found" });
         }
