07980e8ffdd3669f00fe48324a8a88f6a1780c8d
Vikalp Mishra <vikalpmishra783@gmail.com>
2023-09-18 20:52:51 +0530

    fixed attendance table

--- prisma/migrations/20230915070517_fixes_in_leads/migration.sql
+++
@@ -1,8 +0,0 @@
--- DropForeignKey
-ALTER TABLE `leads` DROP FOREIGN KEY `leads_leads_contact_person_id_fkey`;
-
--- AlterTable
-ALTER TABLE `leads` MODIFY `leads_contact_person_id` INTEGER NULL;
-
--- AddForeignKey
-ALTER TABLE `leads` ADD CONSTRAINT `leads_leads_contact_person_id_fkey` FOREIGN KEY (`leads_contact_person_id`) REFERENCES `leads_contact_person`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
